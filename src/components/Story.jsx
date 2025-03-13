e4ad94c1e9a43ffa76a0d9aed139d52bfdb54f55
Richa  Sapre <richasapre@Richas-MacBook-Air-2.local>
2025-03-12 23:08:45 -0700

    Added new content

--- src/components/Story.jsx
+++ src/components/Story.jsx
@@ -49,12 +49,12 @@ const FloatingImage = () => {
     <div id="story" className="min-h-dvh w-screen bg-black text-blue-50">
       <div className="flex size-full flex-col items-center py-10 pb-24">
         <p className="font-general text-sm uppercase md:text-[10px]">
-          the multiversal ip world
+          Current Interests
         </p>
 
         <div className="relative size-full">
           <AnimatedTitle
-            title="the st<b>o</b>ry of <br /> a hidden real<b>m</b>"
+            title="exp<b>a</b>nding <br /> my hor<b>i</b>zon"
             containerClass="mt-5 pointer-events-none mix-blend-difference relative z-10"
           />
 
@@ -67,7 +67,7 @@ const FloatingImage = () => {
                   onMouseLeave={handleMouseLeave}
                   onMouseUp={handleMouseLeave}
                   onMouseEnter={handleMouseLeave}
-                  src="/img/entrance.webp"
+                  src="/img/entrance.png"
                   alt="entrance.webp"
                   className="object-contain"
                 />
@@ -106,14 +106,12 @@ const FloatingImage = () => {
         <div className="-mt-80 flex w-full justify-center md:-mt-64 md:me-44 md:justify-end">
           <div className="flex h-full w-fit flex-col items-center md:items-start">
             <p className="mt-3 max-w-sm text-center font-circular-web text-violet-50 md:text-start">
-              Where realms converge, lies Zentry and the boundless pillar.
-              Discover its secrets and shape your fate amidst infinite
-              opportunities.
+            Eager to explore new technologies and deepen my understanding of the ever-evolving tech landscape. I strive to expand my knowledge through hands-on projects, continuous learning, and staying updated with the latest industry trends.
             </p>
 
             <Button
               id="realm-btn"
-              title="discover prologue"
+              title="VIEW BLOG"
               containerClass="mt-5"
             />
           </div>
