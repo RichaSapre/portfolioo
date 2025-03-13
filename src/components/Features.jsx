1abb6624d87651833b37594ffb12202edf3109b7
Richa  Sapre <richasapre@Richas-MacBook-Air-2.local>
2025-03-13 00:40:49 -0700

    Added correct audio

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -161,7 +161,7 @@ const Features = () => (
         <BentoTilt className="bento-tilt_2">
           <div className="flex size-full flex-col justify-between bg-violet-300 p-5">
             <h1 className="bento-title special-font max-w-64 text-black">
-              More coming soon.
+              Projects coming soon.
             </h1>
             <TiLocationArrow className="m-5 scale-[5] self-end" />
           </div>
